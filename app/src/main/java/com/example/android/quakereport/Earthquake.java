8e9166e863e9568afa665bbde4a3ef9f4a151c1f
Harmeet Singh <sharmeet167@gmail.com>
2019-07-08 15:14:06 +0530

    removed hard coded data

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,36 +1,78 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.quakereport;
 
+/**
+ * An {@link Earthquake} object contains information related to a single earthquake.
+ */
 public class Earthquake {
 
-private Double eMagnitude;
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
 
-private String ePlace;
+    /** Location of the earthquake */
+    private String mLocation;
 
-private long mTimeInMilliseconds;
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
 
-private String eUrl;
+    /** Website URL of the earthquake */
+    private String mUrl;
 
-
-public Earthquake (Double magnitude , String place , long timeInMilliseconds , String url){
-
-    eMagnitude=magnitude;
-    ePlace=place;
-    mTimeInMilliseconds=timeInMilliseconds;
-    eUrl=url;
-}
-
-    public Double geteMagnitude() {
-        return eMagnitude;
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
-    public String getePlace() {
-        return ePlace;
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public double getMagnitude() {
+        return mMagnitude;
     }
 
-    public long getmTimeInMilliseconds(){
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
+        return mLocation;
+    }
+
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
         return mTimeInMilliseconds;
     }
-    public String geteUrl(){
-    return eUrl;
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
     }
 }
